4a49365ef5fae204d6688ff117c4991022e653a5
Dr. Usman Kayani <usman.kayaniphd@gmail.com>
2022-04-15 01:56:54 +0100

    Change package name in setup.py file.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='multilateral_index_calc',
+    name='PriceIndexCalc',
     version='0.1',
-    description='Price Index Calculator',
+    description='Price Index Calculator using bilateral and multilateral methods',
     author='Dr. Usman Kayani',
-    url='https://github.com/drrobotk/multilateral_index_calc',
+    url='https://github.com/drrobotk/PriceIndexCalc',
     author_email='usman.kayani@ons.gov.uk',
     license='MIT',
-    packages=['multilateral_index_calc'],
+    packages=['PriceIndexCalc'],
     install_requires=['pandas', 'numpy', 'scipy'],
     include_package_data=True,
 )
