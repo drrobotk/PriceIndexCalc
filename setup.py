079b2f6a74e214bc8afe8149186063f142b545ff
Dr. Usman Kayani <usman.kayani@ons.gov.uk>
2022-05-12 17:36:31 +0100

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PriceIndexCalc',
-    version='0.1-dev4',
+    version='0.1-dev5',
     description='Price Index Calculator using bilateral and multilateral methods',
     author='Dr. Usman Kayani',
     url='https://github.com/drrobotk/PriceIndexCalc',
